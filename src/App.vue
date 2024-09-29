f549876f3b2f706a1897e5f907cf80eca8f67055
mke <faridunqosim@gmail.com>
2024-09-29 20:00:20 +0500

    Customised the layout of home page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
-import Header from './components/Header.vue';
+import Header from '@/components/Header.vue';
+import Home from '@/pages/Home.vue';
 </script>
 
 <template>
-	<Header />
+	<div class="mx-auto w-full h-screen space-y-3 px-28">
+		<Header />
+		<Home />
+	</div>
 </template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,7 @@
+<script setup>
+import Slider from '@/components/home/Slider.vue';
+</script>
+
+<template>
+	<Slider />
+</template>
