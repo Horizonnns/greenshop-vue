ee0abfce4d1bd182bf14ef66fb5a49c46a99d318
mke <faridunqosim@gmail.com>
2024-09-29 14:51:33 +0500

    Style header block layout - Add logo, navigation menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,46 @@
+<script setup>
+import IconLogo from './icons/IconLogo.vue';
+import IconSearch from './icons/IconSearch.vue';
+import IconCart from './icons/IconCart.vue';
+import IconLogout from './icons/IconLogout.vue';
+
+const navLinks = [
+	{ name: 'Home', href: '/home' },
+	{ name: 'Shop', href: '/shop' },
+	{ name: 'Plant Care', href: '/care' },
+	{ name: 'Blogs', href: '/blogs' },
+];
+</script>
+
 <template>
-	<h2 class="text-3xl font-bold text-center p-5">Header</h2>
+	<header class="flex items-center justify-between border-b mx-28 pt-6 pb-4">
+		<div class="flex items-center space-x-2">
+			<IconLogo />
+			<h2 class="text-xl text-center text-primary">GREENSHOP</h2>
+		</div>
+
+		<nav class="flex items-center space-x-12">
+			<ul
+				v-for="link in navLinks"
+				:key="link.name"
+				class="flex justify-center space-x-5"
+			>
+				<li>
+					<a :href="link.href">{{ link.name }}</a>
+				</li>
+			</ul>
+		</nav>
+
+		<nav class="flex items-center space-x-8">
+			<IconSearch />
+			<IconCart />
+
+			<button
+				class="flex items-center text-white bg-primary rounded-md px-4 py-2 space-x-2"
+			>
+				<IconLogout />
+				<span>Logout</span>
+			</button>
+		</nav>
+	</header>
 </template>
