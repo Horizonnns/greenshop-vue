e3f3978fa10c20647a1ee20e97245bb2139d822a
mke <faridunqosim@gmail.com>
2024-09-29 14:58:13 +0500

    Added necessary colours to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,21 @@ export default {
 	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
 	theme: {
 		extend: {},
+		extend: {
+			colors: {
+				dark: '#3d3d3d',
+				primary: '#46a358',
+				'light-green': '#4aa3581a',
+
+				gray: {
+					10: '#fbfbfb',
+				},
+
+				yellow: {
+					10: '#ffac0c',
+				},
+			},
+		},
 	},
 	plugins: [],
 };
